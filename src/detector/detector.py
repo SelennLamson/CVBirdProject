af7d6a99a02c77d8af2d14da91d4a289e0ebf8e6
Weazel <perso@thomas-lamson.com>
2020-02-05 15:01:25 +0100

    quads: relative edge size check + fixed convexity

--- src/detector/detector.py
+++ src/detector/detector.py
@@ -8,46 +8,88 @@ from .quads import *
 from .markers import *
 
 
-def detect_markers(src_img, max_dim=None, draw_preprocessed=False, draw_corner=False, draw_quads=False):
+class DetectorParameters:
+	def __init__(self):
+		# Preprocessing & corners
+		self.max_dim = None
+		self.apply_bilateral = True
+		self.binary_mode = binary_average
+		self.binary_mask = None
+		self.corners_scale = 10
+		self.max_corners = 500
+		self.corners_quality = 0.01
+
+		# Edges
+		self.edge_samples = 20
+		self.edge_precision = 0.95
+		self.edge_min_dist = 0.01
+		self.edge_max_dist = 0.8
+		self.orthogonal_dist = 0.05
+
+		# Markers
+		self.n_bits = 4
+		self.border = 2
+		self.aruco_dict = cv2.aruco.DICT_4X4_250
+		self.error_border = 0.1
+		self.error_content = 0.05
+
+		# Debug
+		self.draw_preprocessed = False
+		self.draw_binary = False
+		self.draw_mask = False
+		self.draw_corners = False
+		self.draw_quads = False
+		self.return_preview = False
+
+
+def detect_markers(src_img, params: DetectorParameters):
 	"""
 	Locate markers on an RGB image and optionnaly displays an image with markers' perimeter highlighted and detected corners
 	:param src_img:
-	:param max_dim:
-	:param draw_preprocessed:
-	:param draw_corner:
-	:param draw_quads:
+	:param params:
 	:return:
 	"""
 
 	start_time = time.time()
 
-	resized = resize(src_img, max_dim)
-	grayscale, binary = preprocess(resized, apply_bilateral=True, binary_mode=binary_average)
-	corners = identify_corners(grayscale, binary, scale=10, mask=None)
-	quads = detect_quads(binary, corners, samples=20, precision=0.95, min_dist=0.01, max_dist=0.8)
-	bin_mats = extract_binary_matrices(binary, corners, quads, n_bits=8)
-	indexes = binary_check(bin_mats, cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250), 4, 2, 0.1, 0.05)
-	print(indexes)
+	resized = resize(src_img, params.max_dim)
+	grayscale, binary, mask = preprocess(resized, apply_bilateral=params.apply_bilateral, binary_mode=params.binary_mode, binary_mask=params.binary_mask)
+	corners = identify_corners(grayscale, binary, params.corners_scale, params.max_corners, params.corners_quality, mask=mask)
+	quads = detect_quads(binary, corners, samples=params.edge_samples, precision=params.edge_precision, min_dist=params.edge_min_dist, max_dist=params.edge_max_dist, orth_dst=params.orthogonal_dist)
+	bin_mats = extract_binary_matrices(binary, corners, quads, n_bits=params.n_bits + params.border * 2)
+	indexes = binary_check(bin_mats, cv2.aruco.Dictionary_get(params.aruco_dict), params.n_bits, params.border, params.error_border, params.error_content)
 
 	elapsed = time.time() - start_time
 
-	if not (draw_preprocessed or draw_corner or draw_quads):
-		return bin_mats
+	if not (params.draw_preprocessed or
+			params.draw_binary or
+			params.draw_mask or
+			params.draw_corners or
+			params.draw_quads or
+			params.return_preview):
+		return bin_mats, elapsed
 
-	if draw_preprocessed:
+	if params.draw_mask and mask is not None:
+		preview = cv2.cvtColor(mask*255, cv2.COLOR_GRAY2RGB)
+	elif params.draw_binary:
+		preview = cv2.cvtColor(binary*255, cv2.COLOR_GRAY2RGB)
+	elif params.draw_preprocessed:
 		preview = cv2.cvtColor(grayscale, cv2.COLOR_GRAY2RGB)
 	else:
 		preview = resized.copy()
 
-	if draw_corner:
+	if params.draw_corners:
 		preview = draw_corners(preview, corners)
 
-	if draw_quads:
+	if params.draw_quads:
 		for c1, c2, c3, c4 in quads:
 			cv2.line(preview, tuple(corners[c1]), tuple(corners[c2]), (255, 0, 0), 2)
 			cv2.line(preview, tuple(corners[c2]), tuple(corners[c3]), (255, 0, 0), 2)
 			cv2.line(preview, tuple(corners[c3]), tuple(corners[c4]), (255, 0, 0), 2)
 			cv2.line(preview, tuple(corners[c4]), tuple(corners[c1]), (255, 0, 0), 2)
+
+	if params.return_preview:
+		return bin_mats, elapsed, preview
 
 	plt.figure(figsize=(10, 7))
 	plt.imshow(preview)
