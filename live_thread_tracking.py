Fixed errors, changed data for better reference frames, created live_thread_tracking, finished Rotation transform of markers.

live_thread_tracking.py
@@ -1,17 +1,24 @@
-
+import numpy as np
 import threading
 import time
 from typing import List
+import cv2
 
 from src.model.Marker import Marker
+from src.detector.detector import detect_markers
+from src.detector.detector import DetectorParameters
+from src.detector.preprocessing import binary_midgray, intensity_mask
+
 
 class DetectorThread(threading.Thread):
-	def __init__(self, name, w, h, receiver):
+	def __init__(self, name, w, h, cam_no, receiver):
 		super(DetectorThread, self).__init__(name=name)
 		self.stop_event = threading.Event()
+		self.detect_event = threading.Event()
 
 		self.w = w
 		self.h = h
+		self.cam_no = cam_no
 		self.imlen = h * w
 		self.image = np.zeros(self.imlen, dtype=np.uint8)
 		self.head = 0
@@ -31,27 +38,37 @@ class DetectorThread(threading.Thread):
 
 	def run(self):
 		while not self.stop_event.is_set():
+			if self.detect_event.is_set():
+				self.detect_markers()
+				self.detect_event.clear()
 			time.sleep(0.01)
 
 	def stop(self):
 		self.stop_event.set()
+
+	def ask_detection():
+		self.detect_event.set()
 
 	def feed_image(self, feed):
 		flen = len(feed)
 		if self.head + flen >= self.imlen:
 			self.image[self.head:] = feed[:self.imlen - self.head]
 		else:
-			self.image[self.head : self.head + flen] = feed
+			self.image[self.head: self.head + flen] = feed
 
 		self.head += flen
 		if self.head >= self.imlen:
 			self.head = 0
 
 	def detect_markers(self):
-		# Preprocessing
-		gray = self.image.copy().reshape(self.h, self.w)  # Conversion to gray scale
+		# cv2.imwrite('D:/Thomas/UnrealProjects/UE4.22/MarkerDetection/Source/Python/frame{}.png'.format(int((time.time()*1000)%1000)), self.image.copy().reshape(self.h, self.w))
 
-		markers, elapsed = detect_markers(gray, self.params)
+		# Preprocessing
+		# gray = self.image.copy().reshape(self.h, self.w)  # Conversion to gray scale
+		img = cv2.imread(
+			'D:/Thomas/UnrealProjects/UE4.22/MarkerDetection/Source/Python/frame_' + str(self.cam_no) + '.jpg')
+
+		markers, elapsed = detect_markers(img, self.params)
 
 		absolute_location = [0.0, 0.0, 0.0]
 
@@ -59,15 +76,17 @@ class DetectorThread(threading.Thread):
 		# --> 'Results:X,Y,Z|101,x,y,z,rx,ry,rz|3,x,y,z,rx,ry,rz'
 		r = 'Results:'
 		r += ','.join(list(map(str, absolute_location)))
-		for marker in markers:
-			r += '|' + ','.join(list(map(str, [marker.id] + list(marker.pos) + list(marker.rot))))
-
-		self.receiver.receive_result(r[:-1])
+		if len(markers) > 0:
+			for marker in markers:
+				r += '|' + str(marker.id)
+				r += ',' + ','.join(list(map(str, list(marker.pos))))
+				r += ',' + ','.join(list(map(str, list(marker.rot))))
+		self.receiver.receive_result(r)
 
 
 class ThreadManager(object):
-	def __init__(self, width, height):
-		self.worker = DetectorThread("worker", width, height, self)
+	def __init__(self, width, height, cam_no):
+		self.worker = DetectorThread('worker', width, height, cam_no, self)
 		self.result_ready = False
 		self.result = ""
 
@@ -82,19 +101,20 @@ class ThreadManager(object):
 		self.worker.feed_image(feed)
 
 	def detect_markers(self):
-		self.worker.detect_markers()
+		self.worker.detect_event.set()
+		self.result_ready = False
 
 	def receive_result(self, r):
 		self.result = r
 		self.result_ready = True
 
 	def get_result(self):
+		value = "NotReady"
 		was_ready = self.result_ready
 		self.result_ready = False
 
 		if was_ready:
-			return self.result
-		else:
-			return "NotReady"
+			value = self.result
+		return value
 
 